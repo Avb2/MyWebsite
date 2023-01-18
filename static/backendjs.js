e8ec97ccbe0dc5a97c12a48c6e7ad3cbca2ab098
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-01-17 20:36:30 -0500

    Create backendjs.js

---
+++ static/backendjs.js
@@ -0,0 +1,8 @@
+function getNameNumber() {
+            let name = document.getElementById('name').value;
+            let number = document.getElementById('number').value;
+            console.log(`Name: ${name}, Number: ${number}`);
+    document.getElementById('processed').innerHTML='Your information was collected!';
+
+        }
+
